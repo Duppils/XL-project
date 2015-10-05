03b51fae362860b92ba296a70faac5a33c38fd27
Ulf Hörndahl <ulf@ulf-laptop.(none)>
2015-10-05 15:16:29 +0200

    Grunden till controll paketet skapat

---
+++ xl/src/controll/ReturnListener.java
@@ -0,0 +1,25 @@
+import java.awt.event;
+
+import gui.Editor;
+
+public class ReturnListener implements ActionListener {
+	
+	Editor ed;
+	Sheet sheet;
+	CurrentSlot cs;
+	
+	public ReturnListener(Editor ed, Sheet s, CurrentSlot cs) {
+		this.ed = ed;
+		this.spreadsheet = s;
+		this.cs = cs;
+	}
+	
+	actionPerformed(ActionEvent e) {
+		if ( e.getKeyCode() == KeyEvent.VK_ENTER ) {
+			String s = ed.getText();
+			Sheet.setValue( cs.toString(), s )
+			//gör någonting med s... Typ mata vidare in i modellen...
+		}
+	}
+
+}

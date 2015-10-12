217faf16711b9e43330995bd05cd5596f87045d6
Zenche <kanyffel@gmail.com>
2015-10-12 13:32:05 +0200

    ExprSlot method removed, Sheet redunancy trimmed

--- xl/src/spreadsheet/Sheet.java
+++ xl/src/spreadsheet/Sheet.java
@@ -11,71 +11,70 @@ import gui.SheetBase;
 public class Sheet extends SheetBase {
 	private HashMap<String, Slot> map;
 	private SlotBuilder sb;
-	
-	public Sheet(){
+
+	public Sheet() {
 		sb = new SlotBuilder(this);
 		map = new HashMap<String, Slot>();
 	}
 
-	public String getValue(String name) throws XLException{ 
-			try{
-				return map.get(name).toString();
-			}catch(NullPointerException e){
-				System.err.println(e.getMessage());
-			}
-			return "";
-	}
-	
-	public void setValue(String name, String input) throws XLException{
-		if(map.get(name) != null){
-			try{
-				map.put(name, sb.build(""));
-			}catch(XLException e){
-				System.err.println(e.getMessage());
-			}
-			map.put(name, sb.build(input));
+	public String getValue(String name) throws XLException {
+		try {
+			return map.get(name).toString();
+		} catch (NullPointerException e) {
+			System.err.println(e.getMessage());
 		}
+		return "";
 	}
-	
+
+	public void setValue(String name, String input) throws XLException {
+		try {
+			map.put(name, sb.build(""));
+		} catch (XLException e) {
+			System.err.println(e.getMessage());
+		}
+		map.put(name, sb.build(input));
+	}
+
 	@Override
-	public double value(String name){ 
-		try{
+	public double value(String name) {
+		try {
 			return map.get(name).value();
-		}catch(NullPointerException e){
+		} catch (NullPointerException e) {
 			System.err.println(e.getMessage());
 		}
 		return 0;
 	}
-	
+
 	@Override
-	public void load(File f) throws XLException{
+	public void load(File f) throws XLException {
 		Scanner scan = null;
 		try {
 			scan = new Scanner(f);
 		} catch (FileNotFoundException e) {
-			System.err.println(e.getMessage());  
+			System.err.println(e.getMessage());
 		}
-		if(scan != null){
+		if (scan != null) {
 			String[] list;
-			while(scan.hasNext()){
+			while (scan.hasNext()) {
 				list = scan.nextLine().split("=");
 				setValue(list[0], list[1]);
 			}
 		}
 		scan.close();
 	}
-	
+
 	@Override
-	public void save(File f) throws XLException{
+	public void save(File f) throws XLException {
 		PrintWriter p = null;
-		try{
+		try {
 			p = new PrintWriter(f);
-		}catch(FileNotFoundException e){
+		} catch (FileNotFoundException e) {
 			System.err.println(e.getMessage());
 		}
 		StringBuilder sb = new StringBuilder();
-		for(char c = 'A'; c <= 'Z'; c++){  // A1-Z9 dubbelkolla om detta fungerar
-			for(int i = 1; i <= 9; i++){
+		for (char c = 'A'; c <= 'Z'; c++) { // A1-Z9 dubbelkolla om detta
+											// fungerar
+			for (int i = 1; i <= 9; i++) {
 				sb.append(c + i);
 				p.println(getValue(sb.toString()));
 			}
